bcb0e6ff744c23293336f267450c6c9ca75c4a6e
Sebastian Łużyński <sebaluzik@interia.pl>
2018-03-24 15:58:52 +0100

    Add files via upload

--- AppHandler.py
+++ AppHandler.py
@@ -3,9 +3,10 @@ import appJar
 
 def GUI():
     app = appJar.gui("Modeling Project", "800x600")
-    app.addLabelNumericEntry("k")
-    app.addLabelNumericEntry("a")
-    app.addLabelNumericEntry("b")
-    app.addLabelNumericEntry("m")
-    app.addLabelNumericEntry("A")
+    app.addImage("pic", "graf.gif", 0, 0, colspan=2)
+    app.addLabelNumericEntry("k", row=1, column=0)
+    app.addLabelNumericEntry("a", 2, 0)
+    app.addLabelNumericEntry("b", 3, 0)
+    app.addLabelNumericEntry("m", 4, 0)
+    app.addLabelNumericEntry("A", 5, 0)
     return app
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import AppHandler
-
+import math
+import matplotlib.pyplot as plt
+from scipy import signal
+import numpy as np
 
 def submitEvent():
     k = app.getEntry("k")
@@ -7,11 +10,41 @@ def submitEvent():
     b = app.getEntry("b")
     m = app.getEntry("m")
     A = app.getEntry("A")
-    print(a)
+    list = [k, a, b, m, A]
+    if all(list) is not None and all(list) > 0:
+        checkStability(k, a, b, m, A)
+    else:
+        app.warningBox("Error", "Wykryto puste pola, lub wartości mniejsze od zera!")
+
+
+def checkStability(k, a, b, m, A):
+    if a * b > m * k * A:
+        app.infoBox("Stability", "Układ jest stabilny!")
+    else:
+        app.warningBox("Stability", "Układ nie jest stabilny!")
+
+
+def sineWave():
+    arg = (2 * math.pi) / 1000
+    x = []
+    y = []
+    for i in range(2000):
+        x.append(arg * i)
+        y.append(math.sin(arg * i))
+    plt.plot(x, y)
+    plt.show()
+
+def squareWave():
+    t = np.linspace(0, 1, 500, endpoint=False)
+    plt.plot(t, signal.square(2 * math.pi * 5 * t))
+    plt.ylim(-2, 2)
+    plt.show()
 
 
 app = AppHandler.GUI()
-app.addButton("submit", submitEvent)
+app.addButton("Submit", submitEvent, column=1, row=1)
+app.addButton("Show Sin Wave", sineWave, column=1, row=2)
+app.addButton("Show Square Wave", squareWave, column=1, row=3)
 app.go()
 k = 0
 a = 0
